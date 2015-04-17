4767347b77755b3b05e0b735d9f508f46231441d
umair-khanzada <umairkz52@gmail.com>
2015-04-17 17:46:11 +0500

    create SideBar navigation and toggleClass directive

---
+++ client/app/directives/toggleClass/toggleClass.directive.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('blogSystemApp')
+  .directive('toggleClass', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+          element.on('click', function() {
+              var pickClass = attrs.toggleClass;
+            $('body').find('#nav-wrapper').toggleClass(pickClass);
+
+          });
+      }
+    };
+  });
