c6de93641d0551e88d75121cb3b48a1cce8fa9eb
umair-khanzada <umairkz52@gmail.com>
2015-04-20 14:55:57 +0500

    change mongo lab url to local mongo db path and show all (posts in data base) on posts page

--- client/app/new/new.controller.js
+++ client/app/new/new.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('blogSystemApp')
-  .controller('NewCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('NewCtrl', function ($scope, $http) {
+    $scope.createPost = function(){
+        if($scope.postData === '' || $scope.postName === '') {
+            return;
+        }
+        $http.post('api/posts/',{post_content: $scope.postData, post_name: $scope.postName, post_author:$scope.authorName});
+        $scope.postData = '';
+        $scope.postName = '';
+        $scope.authorName = '';
+        console.log('kkkkkkkkk',$scope.postData)
+    }
+
   });
--- client/app/posts/posts.controller.js
+++ client/app/posts/posts.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('blogSystemApp')
-  .controller('PostsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PostsCtrl', function ($scope, $http) {
+        $scope.posts = [];
+        $http.get('/api/posts/').success(function (postList) {
+            $scope.posts = postList ;
+        });
   });
