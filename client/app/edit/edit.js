0e6896781915c414618b8b7887e81e77ad6d5927
Ijlal Khanzada <ijlalkhanzada@gmail.com>
2015-04-20 19:44:57 +0500

    fixed userName and logOut btn in nave header

--- client/app/edit/edit.js
+++ client/app/edit/edit.js
@@ -4,7 +4,7 @@ angular.module('blogSystemApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('edit', {
-        url: '/admin/edit',
+        url: '/admin/post/edit',
         templateUrl: 'app/edit/edit.html',
         controller: 'EditCtrl'
       });
--- client/app/new/new.js
+++ client/app/new/new.js
@@ -4,7 +4,7 @@ angular.module('blogSystemApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('new', {
-        url: '/admin/new',
+        url: '/admin/post/new',
         templateUrl: 'app/new/new.html',
         controller: 'NewCtrl'
       });
