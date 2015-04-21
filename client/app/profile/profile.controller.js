4be3d973554b79774682686aa428323569108f54
Ijlal Khanzada <ijlalkhanzada@gmail.com>
2015-04-21 16:05:00 +0500

    implemented on user detail page

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -12,7 +12,7 @@ angular.module('blogSystemApp')
             if(oldUserName == user.userName && oldEmail == user.email) {
                 $http.put('/api/users/details/' + user._id, {userName: user.userName, email: user.email, firstName: user.firstName, lastName: user.lastName, mobile: user.mobile, age: user.age, city: user.city, country: user.country, gender: user.gender, company: user.company, designation: user.designation, website: user.website, state: user.state, description: user.description});
                 console.log(oldUserName,user.userName, oldEmail, user.email);
-                $state.go('main');
+                $state.go('user');
             }
         }
     });
--- client/app/users/users.controller.js
+++ client/app/users/users.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('blogSystemApp')
-  .controller('UsersCtrl', function ($scope, $http) {
+  .controller('UsersCtrl', function ($scope, $http,$state) {
      $scope.users = [];
         $http.get('/api/users').success(function(usersList){
             $scope.users = usersList;
-        })
+        });
+    $scope.userProfile = function(profile){
+      $state.go('profileUser', {id: profile._id});
+    }
   });
