6bb4392900d3b3848384afeaa3be2bbe55a8e482
umair-khanzada <umairkz52@gmail.com>
2015-04-21 10:40:09 +0500

    add Auth service in post controller

--- client/app/posts/posts.controller.js
+++ client/app/posts/posts.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('blogSystemApp')
-  .controller('PostsCtrl', function ($scope, $http) {
+  .controller('PostsCtrl', function ($scope, $http, Auth) {
         $scope.posts = [];
         $http.get('/api/posts/').success(function (postList) {
             $scope.posts = postList ;
         });
+
   });
