8ee4e39f4b5b3a76351c751bca2a69cb011c74ef
Ijlal Khanzada <ijlalkhanzada@gmail.com>
2015-04-23 19:58:23 +0500

    fixed some Changes for in route

--- client/app/editUser/editUser.controller.js
+++ client/app/editUser/editUser.controller.js
@@ -1,14 +1,15 @@
 'use strict';
 
 angular.module('blogSystemApp')
-  .controller('EditUserCtrl', function ($scope, User) {
-//        $scope.user = User.get();
-//      console.log( User.get());
-        User.getUser();
-//        $scope.userProfile = function(profile){
-//            $scope.user = profile;
-//            $scope.update = function(){
-//                User.updateProfile($scope.user);
-//            }
-//        };
+  .controller('EditUserCtrl', function ($scope, User, $state) {
+        User.getUser({id: $state.params.id}, function(user){
+          console.log(user)
+          $scope.user = user;
+        });
+        $scope.userProfile = function(profile){
+            $scope.user = profile;
+            $scope.update = function(){
+                User.updateProfile($scope.user);
+            }
+        };
   });
