d38c25432dd56862c72cbcd5f4bd4ff9cc7a927d
umair-khanzada <umairkz52@gmail.com>
2015-05-05 14:47:09 +0500

    apply conditions on add comments

--- client/app/main/view/view.controller.js
+++ client/app/main/view/view.controller.js
@@ -14,6 +14,7 @@ angular.module('blogSystemApp')
 
         $scope.comment = {};
         $scope.addComment = function(){
+            if(Auth.isLoggedIn() === true) {
                 $scope.comment.comment_post_id = $state.params.id;
                 comment.save($scope.comment, function(res){
                     comment.query({id: $state.params.id}, function(list){
@@ -21,6 +22,10 @@ angular.module('blogSystemApp')
                         $scope.comment = {};
                     });
                 });
+            }
+            else{
+                $scope.errorMess = true;
+            }
         };
 
         $scope.replycomment = [];
@@ -40,9 +45,15 @@ angular.module('blogSystemApp')
         };
 
         $scope.replyComment = function(){
-            comment.save($scope.commentData, function(res){
-                $scope.replycomment.push(res);
-                $scope.commentData.comment_content = '';
-            });
+            if(Auth.isLoggedIn() === true) {
+                console.log(Auth.isLoggedIn());
+                comment.save($scope.commentData, function (res) {
+                    $scope.replycomment.push(res);
+                    $scope.commentData.comment_content = '';
+                });
+            }
+            else{
+                $scope.errorMessage = true;
+            }
         }
   });
