5e47847504892076b7a17513b6b0205068dc5d80
umair-khanzada <umairkz52@gmail.com>
2015-05-08 11:02:49 +0500

    add image option on create new post page

--- server/api/post/index.js
+++ server/api/post/index.js
@@ -4,9 +4,12 @@ var express = require('express');
 var controller = require('./post.controller');
 var config = require('../../config/environment');
 var auth = require('../../auth/auth.service');
+var multipart = require('connect-multiparty')
+    , multipartMiddleware = multipart();
 
 var router = express.Router();
 
+router.post('/upload/url', auth.hasRole('author'), multipartMiddleware, controller.postImageUpload);
 //router.get('/', auth.hasRole('editor'), controller.index);
 router.get('/', controller.index);
 router.get('/:id',auth.hasRole('author'), controller.showAuthorPost);
